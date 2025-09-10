6103329685201a6b517c2e209ebf2f240de07da2
dkswhdgh1018 <zzongho1018@gmail.com>
2025-09-10 14:16:58 +0900

    발도 수정

--- Assets/Script/Player/PlayerState/Player_BaldoState.cs
+++ Assets/Script/Player/PlayerState/Player_BaldoState.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player_BaldoState : PlayerState
 {
     private int baldoDir;
-    private float stateTimer; // 상태 유지 시간을 위한 타이머
+    private float stateTimer; 
 
     public Player_BaldoState(Player player, StateMachine statemachine, string animBoolName) : base(player, statemachine, animBoolName)
     {
@@ -11,29 +11,29 @@ public class Player_BaldoState : PlayerState
 
     public override void Enter()
     {
+        //base.Enter();
         stateTimer = 0.5f; 
         
-        // The player might be idle but holding a direction key.
-        // We must ensure the character is facing the correct way.
+       
         if (player.moveInput.x != 0 && player.moveInput.x != player.facingDir)
         {
             player.Flip();
         }
-
-        // Now, player.facingDir is guaranteed to be correct.
+        player.SetVelocity(0, rb.linearVelocity.y);
+        
         player.skillManager.baldo.UseSkill(player.anim, player.facingDir);
     }
 
     public override void Exit()
     {
-        base.Exit();
+        //base.Exit();
     }
 
     public override void Update()
     {
         base.Update();
-
-        // 타이머가 다 되면 Idle 상태로 변경
+        player.SetVelocity(0, rb.linearVelocity.y);
+        
         if (stateTimer < 0)
             stateMachine.ChangeState(player.idleState);
         else
